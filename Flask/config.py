c5d64ab6c6d4a10daf59a74bc806fcd1e2de0432
igoortech <57847305+igoortech@users.noreply.github.com>
2020-08-04 18:36:39 -0300

    sql driver

--- Flask/config.py
+++ Flask/config.py
@@ -1,3 +1,5 @@
+import pyodbc 
+
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 CORS_HEADERS = 'Content-Type'
@@ -8,7 +10,12 @@ DB = "ipdb"
 USER = "ipeadmin"
 PWD = "qwerty123!@#"
 
-SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{USER}:{PWD}@{SERVER}/{DB}?driver=SQL+Server"
+
+drivers = [item for item in pyodbc.drivers()]
+driver = drivers[-1]
+print("driver:{}".format(driver))
+
+SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{USER}:{PWD}@{SERVER}/{DB}?driver={driver}"
 IP_PERMITIDO = "127.0.0.1"
 
 #"mssql+pyodbc://dbipe.database.windows.net/ipdb?driver=SQL+Server?UID=ipeadmin;PWD=qwerty123!@#;Encrypt=True;TrustServerCertificate=False" 
